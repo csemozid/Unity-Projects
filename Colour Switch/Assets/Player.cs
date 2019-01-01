0158bdcb28c5bdee72ae3369af5136edc4d8cac9
csemozid <csemozid@gmail.com>
2019-01-02 03:19:34 +0600

    Files added

---
+++ Colour Switch/Assets/Player.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    public float jumpForce = 10f;
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+
+
+    public string currentColor;
+
+    public Color colorCyan;
+    public Color colorYellow;
+    public Color colorMagenta;
+    public Color colorPink;
+
+    void Start()
+    {
+        SetRandomColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+    }
+
+    public int coinValue = 1;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Coin")
+        {
+            ScoreManager.instance.ChangeScore(coinValue);
+            Destroy(collision.gameObject);
+
+        }
+        /*
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.instance.ChangeScore(coinValue);
+        }
+        */
+        if (collision.tag == "ColorChanger")
+        {
+            SetRandomColor();
+            Destroy(collision.gameObject);
+            return;
+        }
+        if (collision.tag != currentColor && collision.tag!="Coin")
+        {
+            Debug.Log("GAME OVER, Brother!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+    }
+
+    void SetRandomColor()
+    {
+        int index = Random.Range(0, 4);
+        switch (index)
+        {
+            case 0:
+                currentColor = "Cyan";
+                sr.color = colorCyan;
+                break;
+            case 1:
+                currentColor = "Yellow";
+                sr.color = colorYellow;
+                break;
+            case 2:
+                currentColor = "Magenta";
+                sr.color = colorMagenta;
+                break;
+            case 3:
+                currentColor = "Pink";
+                sr.color = colorPink;
+                break;
+
+        }
+    }
+}
